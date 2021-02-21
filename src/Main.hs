e9cb696972bc4e652986ecc4debbb8f7eb98b23d
Juan <jibarozzibehr@gmail.com>
2021-02-20 23:28:39 -0300

    New main screen and menus

--- src/Main.hs
+++ src/Main.hs
@@ -48,12 +48,13 @@ data Yvalue = Arriba | Abajo
 
 data UI = UI
     {   _game   :: Game
-    ,   _paused :: Bool
     ,   _barPlayerOne   :: Location
     ,   _barPlayerTwo   :: Location
     ,   _ball           :: Location
     ,   _xBall          :: Xvalue
     ,   _yBall          :: Yvalue
+    ,   _status         :: Int      --  0: Pausa, 1: Inicio, 2: Elegir nivel, 3: Jugando
+    ,   _previousStatus :: Int      --  1 por defecto.
     }
 makeLenses ''UI
 
@@ -75,13 +76,46 @@ drawUI ui   =
     --    borderWithLabel (str "Pong") $ (str "Puntaje jugador 1: " <+> str (show (ui ^. game ^. scorePlayerOne))) <+> 
     --        (str "Puntaje jugador 2: " <+> str (show (ui ^. game ^. scorePlayerTwo)))
     --]
-    
-    [ rightBar ui
-    , leftBar ui
-    , ballDraw ui
-    , vLimit 24 $ drawGrid ui
-    ]
+    case ui ^. status of
+        0   -> 
+                [paused]
+        1   ->
+                [mainScreen]
+        3   ->
+                [   rightBar ui
+                ,   leftBar ui
+                ,   ballDraw ui
+                ,   playing
+                ]
+        where
+            paused  = hLimit 80
+                    $ withBorderStyle unicode
+                    $ borderWithLabel (str "Pong")
+                    $ center $ str "Game paused"
+            playing = hLimit 80
+                    $ withBorderStyle unicode
+                    $ borderWithLabel (str "Pong")
+                    $ (padLeft Max (str (show $ ui ^. game ^. scorePlayerOne)) <+> vBorder <+> padRight Max (str (show $ ui ^. game ^. scorePlayerTwo)))
+            mainScreen  =   (   hLimit 80
+                                $ withBorderStyle unicode
+                                $ border
+                                $ center pongTitle
+                                <=> (hCenter $ str "a version by Butros Asis and Juan Barozzi")
+                                <=> (hCenter $ str "\n(1): Play classic\n(2): Play against the machine\n(3): Play against the wall")
+                            )
+                            
 
+                    
+    
+    
+    --[ rightBar ui
+    --, leftBar ui
+    --, ballDraw ui
+    --, vLimit 24 $ drawGrid ui
+    --]
+
+pongTitle :: Widget Name
+pongTitle = str "         _               _                _                   _        \n        /\\ \\            /\\ \\             /\\ \\     _          /\\ \\      \n       /  \\ \\          /  \\ \\           /  \\ \\   /\\_\\       /  \\ \\     \n      / /\\ \\ \\        / /\\ \\ \\         / /\\ \\ \\_/ / /      / /\\ \\_\\    \n     / / /\\ \\_\\      / / /\\ \\ \\       / / /\\ \\___/ /      / / /\\/_/    \n    / / /_/ / /     / / /  \\ \\_\\     / / /  \\/____/      / / / ______  \n   / / /__\\/ /     / / /   / / /    / / /    / / /      / / / /\\_____\\ \n  / / /_____/     / / /   / / /    / / /    / / /      / / /  \\/____ / \n / / /           / / /___/ / /    / / /    / / /      / / /_____/ / /  \n/ / /           / / /____\\/ /    / / /    / / /      / / /______\\/ /   \n\\/_/            \\/_________/     \\/_/     \\/_/       \\/___________/     \n"
 
 rightBar :: UI -> Widget Name
 rightBar ui =
@@ -98,15 +132,15 @@ ballDraw ui =
     translateBy (ui ^. ball) $
     str "---"
 
-drawGrid :: UI -> Widget Name
-drawGrid ui =
-    hLimit 80
-        $ withBorderStyle unicode
-        $ borderWithLabel (str "Pong")
-        $ case ui ^. paused of
-            True    -> center $ str "Juego en pausa"
-            --False   -> (center $ str "Puntaje jugador 1: " <+> str (show $ ui ^. game ^. scorePlayerOne))
-            False   -> (padLeft Max (str (show $ ui ^. game ^. scorePlayerOne)) <+> vBorder <+> padRight Max (str (show $ ui ^. game ^. scorePlayerTwo)))
+--  drawGrid :: UI -> Widget Name
+--  drawGrid ui =
+--      hLimit 80
+--          $ withBorderStyle unicode
+--          $ borderWithLabel (str "Pong")
+--          $ case ui ^. paused of
+--              True    -> center $ str "Juego en pausa"
+--              --False   -> (center $ str "Puntaje jugador 1: " <+> str (show $ ui ^. game ^. scorePlayerOne))
+--              False   -> (padLeft Max (str (show $ ui ^. game ^. scorePlayerOne)) <+> vBorder <+> padRight Max (str (show $ ui ^. game ^. scorePlayerTwo)))
 
 ballUp :: UI -> UI
 ballUp ui =
@@ -126,16 +160,17 @@ ballRight ui =
 
 handleTick :: UI -> EventM Name (Next UI)
 handleTick ui =
-    if ui ^. paused 
+    if ui ^. status == 0    --  Si está pausado
     then continue ui
     else case ui ^. ball . locationRowL of
-            0    -> continue $ uiFinal $ bordeSuperior $ tocaBarra ui
+            0   -> continue $ uiFinal $ bordeSuperior $ tocaBarra ui
             --False   -> (center $ str "Puntaje jugador 1: " <+> str (show $ ui ^. game ^. scorePlayerOne))
-            23   -> continue $ uiFinal $ bordeInferior $ tocaBarra ui
-            _      -> continue $ uiFinal $ tocaBarra ui
+            23  -> continue $ uiFinal $ bordeInferior $ tocaBarra ui
+            _   -> continue $ uiFinal $ tocaBarra ui
 
-        where uiHorizontal ui'= if (ui' ^. xBall) == Izquierda then ballLeft ui' else ballRight ui'
-              uiFinal ui' = if (ui' ^. yBall) == Arriba then ballUp $ uiHorizontal ui' else ballDown $ uiHorizontal ui'
+            where
+                uiHorizontal ui'= if (ui' ^. xBall) == Izquierda then ballLeft ui' else ballRight ui'
+                uiFinal ui' = if (ui' ^. yBall) == Arriba then ballUp $ uiHorizontal ui' else ballDown $ uiHorizontal ui'
 
 
 
@@ -156,7 +191,7 @@ tocaBarra ui =
     if ((ui ^. ball . locationRowL >= ui ^. barPlayerOne . locationRowL) && (ui ^. ball . locationRowL <= (ui ^. barPlayerOne . locationRowL)+5)) && (ui ^. ball . locationColumnL == (ui ^. barPlayerOne . locationColumnL)+2)
     then ui & xBall .~ Derecha
     else 
-        if ((ui ^. ball . locationRowL >= ui ^. barPlayerTwo . locationRowL) && (ui ^. ball . locationRowL <= (ui ^. barPlayerTwo . locationRowL)+5)) && (ui ^. ball . locationColumnL == (ui ^. barPlayerTwo . locationColumnL))
+        if ((ui ^. ball . locationRowL >= ui ^. barPlayerTwo . locationRowL) && (ui ^. ball . locationRowL <= (ui ^. barPlayerTwo . locationRowL)+5)) && (ui ^. ball . locationColumnL == (ui ^. barPlayerTwo . locationColumnL)-2)
         then ui & xBall .~ Izquierda
         else ui
 
@@ -170,41 +205,90 @@ bordeInferior ui = ui & yBall .~ Arriba
 
 
 handleEvent :: UI -> BrickEvent Name Tick -> EventM Name (Next UI)
-handleEvent ui (AppEvent Tick)  = handleTick ui
+handleEvent ui event =
+    case ui ^. status of
+        --  Juego pausado
+        0   -> case event of
+                    (VtyEvent (V.EvKey (V.KChar 'p') []))   -> continue pause
+                    (VtyEvent (V.EvKey (V.KChar 'P') []))   -> continue pause
+                    (VtyEvent (V.EvKey (V.KChar 'q') []))   -> halt ui
+                    (VtyEvent (V.EvKey (V.KChar 'Q') []))   -> halt ui
+                    _                                       -> continue ui
+                    where
+                        pause = ui & status .~ (ui ^. previousStatus)    -- Acá debería ir al estado anterior.
+        --  Pantalla principal
+        1   -> case event of
+                    (VtyEvent (V.EvKey (V.KChar '1') []))   -> continue playClassic
+                    (VtyEvent (V.EvKey (V.KChar '2') []))   -> continue playMachine
+                    (VtyEvent (V.EvKey (V.KChar '3') []))   -> continue playWall
+                    (VtyEvent (V.EvKey (V.KChar 'q') []))   -> halt ui
+                    (VtyEvent (V.EvKey (V.KChar 'Q') []))   -> halt ui
+                    _                                       -> continue ui
+                    where
+                        playClassic = ui & status .~ 3
+                        playMachine = undefined
+                        playWall    = undefined
+        --  Elegir nivel
+        2   -> continue ui
+        --  Jugando classic
+        3   -> case event of
+                    (VtyEvent (V.EvKey (V.KChar 'p') []))   -> continue pause
+                    (VtyEvent (V.EvKey (V.KChar 'P') []))   -> continue pause
+                    (VtyEvent (V.EvKey (V.KChar 'q') []))   -> halt ui
+                    (VtyEvent (V.EvKey (V.KChar 'Q') []))   -> halt ui
+                    --  Tick
+                    (AppEvent Tick)                         -> handleTick ui
+                    --  Controles
+                    (VtyEvent (V.EvKey V.KDown []))         -> continue func2
+                    (VtyEvent (V.EvKey V.KUp []))           -> continue func3
+                    (VtyEvent (V.EvKey (V.KChar 's') []))   -> continue func4
+                    (VtyEvent (V.EvKey (V.KChar 'S') []))   -> continue func4
+                    (VtyEvent (V.EvKey (V.KChar 'w') []))   -> continue func5
+                    (VtyEvent (V.EvKey (V.KChar 'W') []))   -> continue func5
+                    --  Cualquier otra tecla no hace nada
+                    _                                   -> continue ui
+                    where
+                        pause = ui & status .~ 0
+                        func2 = if (ui ^. barPlayerTwo . locationRowL) < 18 then ui & barPlayerTwo . locationRowL %~ (+ 1) else ui     
+                        func3 = if (ui ^. barPlayerTwo . locationRowL) > 0 then ui & barPlayerTwo . locationRowL %~ (subtract 1) else ui
+                        func4 = if (ui ^. barPlayerOne . locationRowL) < 18 then ui & barPlayerOne . locationRowL %~ (+ 1) else ui
+                        func5 = if (ui ^. barPlayerOne . locationRowL) > 0 then ui & barPlayerOne . locationRowL %~ (subtract 1) else ui
+        _   -> halt ui
+                    
 
-handleEvent ui (VtyEvent (V.EvKey (V.KChar 'p') [])) = continue func
-	where
-		func = if ui ^. paused then ui & paused .~ False else ui & paused .~ True
-handleEvent ui (VtyEvent (V.EvKey (V.KChar 'P') [])) = continue func
-	where
-		func = if ui ^. paused then ui & paused .~ False else ui & paused .~ True
+--  handleEvent ui (AppEvent Tick)  = handleTick ui
 
-handleEvent ui (VtyEvent (V.EvKey V.KDown [])) = continue func2
-	where
-		func2 = if (ui ^. barPlayerTwo . locationRowL) < 18 then ui & barPlayerTwo . locationRowL %~ (+ 1) else ui
+--  handleEvent ui (VtyEvent (V.EvKey (V.KChar 'p') [])) = continue func
+--  	where
+--  		func = if ui ^. paused then ui & paused .~ False else ui & paused .~ True
 
---continue $ ui ^. game ^. st & barPlayerTwo.locationRowL .~ (+ 1)
+--  handleEvent ui (VtyEvent (V.EvKey (V.KChar 'P') [])) = continue func
+--  	where
+--  		func = if ui ^. paused then ui & paused .~ False else ui & paused .~ True
 
-handleEvent ui (VtyEvent (V.EvKey V.KUp [])) = continue func2
-	where
-		func2 = if (ui ^. barPlayerTwo . locationRowL) > 0 then ui & barPlayerTwo . locationRowL %~ (subtract 1) else ui
+--  handleEvent ui (VtyEvent (V.EvKey V.KDown [])) = continue func2
+--  	where
+--  		func2 = if (ui ^. barPlayerTwo . locationRowL) < 18 then ui & barPlayerTwo . locationRowL %~ (+ 1) else ui
 
---continue $ ui ^. game ^. st & barPlayerTwo.locationRowL .~ (subtract 1)
+--  handleEvent ui (VtyEvent (V.EvKey V.KUp [])) = continue func2
+--  	where
+--  		func2 = if (ui ^. barPlayerTwo . locationRowL) > 0 then ui & barPlayerTwo . locationRowL %~ (subtract 1) else ui
 
-handleEvent ui (VtyEvent (V.EvKey (V.KChar 's') [])) = continue func2
-	where
-		func2 = if (ui ^. barPlayerOne . locationRowL) < 18 then ui & barPlayerOne . locationRowL %~ (+ 1) else ui
-handleEvent ui (VtyEvent (V.EvKey (V.KChar 'S') [])) = continue func2
-	where
-		func2 = if (ui ^. barPlayerOne . locationRowL) < 18 then ui & barPlayerOne . locationRowL %~ (+ 1) else ui
+--  handleEvent ui (VtyEvent (V.EvKey (V.KChar 's') [])) = continue func2
+--  	where
+--  		func2 = if (ui ^. barPlayerOne . locationRowL) < 18 then ui & barPlayerOne . locationRowL %~ (+ 1) else ui
 
---continue $ ui ^. game ^. st & barPlayerTwo.locationRowL .~ (+ 1)
-handleEvent ui (VtyEvent (V.EvKey (V.KChar 'w') [])) = continue func2
-	where
-	    func2 = if (ui ^. barPlayerOne . locationRowL) > 0 then ui & barPlayerOne . locationRowL %~ (subtract 1) else ui
-handleEvent ui (VtyEvent (V.EvKey (V.KChar 'W') [])) = continue func2
-	where
-		func2 = if (ui ^. barPlayerOne . locationRowL) > 0 then ui & barPlayerOne . locationRowL %~ (subtract 1) else ui
+--  handleEvent ui (VtyEvent (V.EvKey (V.KChar 'S') [])) = continue func2
+--  	where
+--  		func2 = if (ui ^. barPlayerOne . locationRowL) < 18 then ui & barPlayerOne . locationRowL %~ (+ 1) else ui
+
+--  handleEvent ui (VtyEvent (V.EvKey (V.KChar 'w') [])) = continue func2
+--  	where
+--  	    func2 = if (ui ^. barPlayerOne . locationRowL) > 0 then ui & barPlayerOne . locationRowL %~ (subtract 1) else ui
+
+--  handleEvent ui (VtyEvent (V.EvKey (V.KChar 'W') [])) = continue func2
+--  	where
+--  		func2 = if (ui ^. barPlayerOne . locationRowL) > 0 then ui & barPlayerOne . locationRowL %~ (subtract 1) else ui
 
 
 theMap :: AttrMap
@@ -236,13 +320,14 @@ playGame = do
     initialVty <- buildVty
     ui <- customMain initialVty buildVty (Just chan) app UI
     --ui <- defaultMain app $ UI
-        { _game    = initialGame
-        , _paused  = False
-        , _barPlayerOne = Location (2, 9)
-        , _barPlayerTwo = Location (75, 9)
-        , _ball         = Location (39, 12)
-        , _xBall        = Izquierda
-        , _yBall        = Arriba
+        { _game             = initialGame
+        , _barPlayerOne     = Location (2, 9)
+        , _barPlayerTwo     = Location (75, 9)
+        , _ball             = Location (39, 12)
+        , _xBall            = Izquierda
+        , _yBall            = Arriba
+        , _status           = 1
+        , _previousStatus   = 3
         }
     return $ ui ^. game
     --  g <- withBorderStyle unicode $
